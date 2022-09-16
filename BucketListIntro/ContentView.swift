051af4e626b4d9906c6df2870a88f5035b99421c
Jacek Kosiński <kashiash@gmail.com>
2022-09-16 23:42:54 +0200

    more places

--- BucketListIntro/ContentView.swift
+++ BucketListIntro/ContentView.swift
@@ -22,22 +22,34 @@ struct ContentView: View {
   let locations = [
     Location(name: "Gabos S.A.", coordinate: CLLocationCoordinate2D(latitude: 50.24839, longitude: 18.99989)),
     Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-    Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076)),
-    Location(name: "PISDZIELCE", coordinate: CLLocationCoordinate2D(latitude: 52.22414, longitude: 20.99068))
+    Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076)),
+    Location(name: "PISDZIELCE", coordinate: CLLocationCoordinate2D(latitude: 52.22414, longitude: 20.99068)),
+    Location(name: "Mysterious Pattern", coordinate: CLLocationCoordinate2D(latitude: 37.629562, longitude: -116.849556)),
+    Location(name: "Uluru", coordinate: CLLocationCoordinate2D(latitude: -25.344375, longitude: 131.034401)),
+    Location(name: "Grand Prismatic Spring", coordinate: CLLocationCoordinate2D(latitude: 44.525049, longitude: -110.83819))
+    
     
   ]
     
     var body: some View {
-        Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-            MapAnnotation(coordinate: location.coordinate){
-                VStack{
-                    Circle()
-                        .stroke(.red,lineWidth: 3)
-                        .frame(width:44,height:44)
-                    Text(location.name)
+        NavigationView{
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate){
+                    NavigationLink{
+                        Text(location.name)
+                    } label: {
+                        VStack{
+                            Circle()
+                                .stroke(.red,lineWidth: 3)
+                                .frame(width:44,height:44)
+                            Text(location.name)
+                        }
+                    }
                 }
             }
-        }
+            
+            
+        }.navigationTitle("World Explorer")
     }
 }
 
