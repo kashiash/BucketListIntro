77fc02526f2a39a7ec616be938e1bc152c5dfe42
Jacek Kosiński <kashiash@gmail.com>
2022-09-16 21:34:45 +0200

    Initial Commit

---
+++ BucketListIntro/BucketListIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListIntroApp.swift
+//  BucketListIntro
+//
+//  Created by Jacek Kosinski U on 16/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
